8914ee9a05afc16d2edd1f8f17469af96aa3c71d
cometoflegend <irenesg83@educastur.es>
2024-11-25 18:14:42 +0100

    ejercicio 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    for (i in 1..100) {
+        if ((i%3!=0) && (i%5!=0)) { println(i) }
+
+        else if ((i%3==0) && (i%5!=0)) { println("tri") }
+
+        else if ((i%3!=0) && (i%5==0)) { println("qui") }
+
+        else if ((i%3==0) && (i%5==0)) { println("triqui") }
+
+
+
     }
 }
