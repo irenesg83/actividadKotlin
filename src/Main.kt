f6c46f55a79153b0e5233c9ad2e792dfcaf381e1
cometoflegend <irenesg83@educastur.es>
2024-11-25 18:28:35 +0100

    limpieza de código, añadido comentario

--- src/Main.kt
+++ src/Main.kt
@@ -2,17 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    var palabra1: String
-    var palabra2: String
-
     println("Introduce una palabra")
-    palabra1=readln()
+    val palabra1: String = readln()
 
     println("Introduce otra palabra")
-    palabra2=readln()
+    val palabra2: String = readln()
 
     println("¿Son $palabra1 y $palabra2 anagramas? ")
 
+    // comprueba si palabra1 y palabra2 son anagramas usando un equals.(String.reversed()). si palabra2 al revés es igual que palabra1, imprime true, si no, imprime false
     if (palabra1.equals(palabra2.reversed())) { print(true) }
     else print(false)
 
