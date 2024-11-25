760fa50a1ce420a6d4efcb47e1533d6b3249e691
cometoflegend <irenesg83@educastur.es>
2024-11-25 18:42:42 +0100

    añadidos más comentarios

--- src/Main.kt
+++ src/Main.kt
@@ -2,16 +2,25 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
+    /*
+    
+    declaro las variables en junto al readln por limpieza, pero se pueden declarar por separado.
+    están declaradas como val porque nunca se modifican.
+    las paso a mayúsculas porque mi método para comprobar si son iguales distingue entre upper y lowercase.
+
+     */
     println("Introduce una palabra")
-    val palabra1: String = readln()
+    val palabra1: String = readln().uppercase()
 
     println("Introduce otra palabra")
-    val palabra2: String = readln()
+    val palabra2: String = readln().uppercase()
 
     println("¿Son $palabra1 y $palabra2 anagramas? ")
 
-    // comprueba si palabra1 y palabra2 son anagramas usando un equals.(String.reversed()). si palabra2 al revés es igual que palabra1, imprime true, si no, imprime false
+    // comprueba si palabra1 y palabra2 son anagramas usando equals y String.reversed(). si palabra2 al revés es igual que palabra1, imprime true, si no, imprime false
     if (palabra1.equals(palabra2.reversed())) { print(true) }
     else print(false)
+
+    // me imagino que también se puede hacer con arrays, añadiendo la primera letra de palabra2 a la última posición, la segunda a la penúltima, etc.. pero sería un lío
 
 }
